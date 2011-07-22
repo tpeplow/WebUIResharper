0b74bda5306f95268330f0c8f7c5e22056968188
no one <nobody@example.com>
2011-07-22 15:07:03 +0100

    Added refactoring option which creates the stub at the same time as the dependency

--- AddDependency/AddDependencyWorkflowProvider.cs
+++ AddDependency/AddDependencyWorkflowProvider.cs
@@ -5,11 +5,12 @@ using JetBrains.ReSharper.Refactorings.Workflow;
 namespace WebUIResharper.AddDependency
 {
     [RefactoringWorkflowProvider]
-    public class AddDependencyWorkflowProvider: IRefactoringWorkflowProvider
+    public class AddDependencyWorkflowProvider : IRefactoringWorkflowProvider
     {
         public IEnumerable<IRefactoringWorkflow> CreateWorkflow(IDataContext dataContext)
         {
             yield return new AddDependencyWorkflow();
+            yield return new AddDependencyWorkflowWithStub();
         }
     }
 }
